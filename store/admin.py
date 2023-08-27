b91bacc53ad5a3b9aa8e8a71cd0421a4f4c528d7
Mohammed Shareef <shareef.mk03@gmail.com>
2023-08-27 14:31:06 +0530

    second

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product,Category,Subcategory
+from .models import Product,Category,Subcategory,Material
 
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Subcategory)
+admin.site.register(Material)
--- store/models.py
+++ store/models.py
@@ -15,12 +15,40 @@ class Subcategory(models.Model):
 
     def __str__(self):
         return self.name
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    
+class Material(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    detail = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    offer_price = models.CharField(max_length=100,blank=True, null=True)
+    brand = models.CharField(max_length=100,blank=True, null=True)
+    diamension = models.CharField(max_length=50,blank=True, null=True)
+    material = models.ManyToManyField(Material)
+    image1 = models.ImageField(upload_to= 'photos/products',null=True,blank=True)
+    image2 = models.ImageField(upload_to= 'photos/products', null=True,blank=True)
+    image3 = models.ImageField(upload_to= 'photos/products',null=True,blank=True)
+    image4 = models.ImageField(upload_to= 'photos/products',null=True,blank=True)
+    image5 = models.ImageField(upload_to= 'photos/products',null=True,blank=True)
+    image6 = models.ImageField(upload_to= 'photos/products',null=True,blank=True)
+    video_file = models.FileField(upload_to='videos/',null = True,blank=True)
+    manufacturer = models.CharField(max_length= 200,blank=True, null=True)
+    in_stock = models.BooleanField(default=False)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    # specific room type field - many to many
+    # home page display --check
+
+    def __str__(self):
+        return self.name
+
+
+# services
+
+# interior stories
+
+# clients page
